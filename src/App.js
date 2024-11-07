547a2c9d76e87d5675cd881237be7d6ad728afb3
113333 <ed113333@edah.org.tw>
2024-11-07 15:46:27 +0800

    88888

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          000.0000.0000.00上不去!!!!!!!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
